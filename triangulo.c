8666023bff491e33971e610e8c105281b7817068
pedro lavor <pedrinho.lavor@aluno.uece.br>
2024-07-16 18:08:55 -0300

    implementação-calculate

--- triangulo.c
+++ triangulo.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
+float calculate(float a, float b);
+
 int main(void)
 {
-    int i;
-    int triangulo[2];
+    float base, altura;
 
-    for (i = 0; i < 2; i++)
-    {
-        printf("Valor %i: ", i + 1);
-        scanf("%i", &triangulo[i]);
-    }
-    printf("O valor da área do triângulo é: %.2f", (triangulo[0] * triangulo[1]) / 2.0);
+    printf("Qual o valor da base? ");
+    scanf("%f", &base);
+
+    printf("Qual o valor da altura? ");
+    scanf("%f", &altura);
+
+    calculate(base, altura);
     return 0;
+}
+
+float calculate(float a, float b)
+{
+    printf("O valor da área é: %f\n", (a * b) / 2.0);
 }
